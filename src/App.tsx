c3b67bfed6031e2da58f2cf54db4194bcd3e9650
Ankit Varshney <an225884@dal.ca>
2025-03-16 15:42:34 -0400

    Added base path

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/united-dairy/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Products />} />
